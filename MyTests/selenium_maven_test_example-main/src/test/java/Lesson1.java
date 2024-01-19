765058786e25c6f298671726fa18f48ab0538945
Abirva1g <106960263+Abirva1g@users.noreply.github.com>
2024-01-19 15:36:57 +0300

    Создание

---
+++ MyTests/selenium_maven_test_example-main/src/test/java/Lesson1.java
@@ -0,0 +1,11 @@
+import org.junit.Before;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
+import java.util.Map;
+
+public class Lesson1 {private WebDriver driver;
+    private Map<String, Object> vars;
+    JavascriptExecutor js;
+    @Beforeфыв
+}
