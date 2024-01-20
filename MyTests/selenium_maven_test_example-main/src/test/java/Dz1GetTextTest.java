0c38a1e36163d8be764ddf50ff629a2d7f285cea
Abirva1g <106960263+Abirva1g@users.noreply.github.com>
2024-01-20 17:49:53 +0300

    rename dz

--- MyTests/selenium_maven_test_example-main/src/test/java/Dz1GetTextTest.java
+++ MyTests/selenium_maven_test_example-main/src/test/java/Dz1GetTextTest.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class GetTextTest {
+public class Dz1GetTextTest {
 
     @Test
     public void GetText() {
