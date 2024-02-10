b30224655241f67279d5113fbcaecd887f4d76d8
Abirva1g <106960263+Abirva1g@users.noreply.github.com>
2024-02-10 21:25:10 +0300

    Dz11

---
+++ MyTests/selenium_maven_test_example-main/src/main/Code/AnotherPackage/DzElevenMethods.java
@@ -0,0 +1,28 @@
+package Code.AnotherPackage;
+
+public class DzElevenMethods {
+    public static void First() {
+        System.out.println("Я помню чудное мгновение");
+        System.out.println("Передо мной явилась ты");
+        System.out.println("Как мимолетное видение");
+        System.out.println("Как гений чистой красоты!");
+    }
+    public static void Second() {
+        String word1 = "Java";
+        String word2 = "лучший";
+        String word3 = "язык";
+        String word4 = "программирования";
+        System.out.println(String.join(" ",word1,"-",word2,word3,word4,"!"));
+    }
+    public static void Third(){
+        String textLang = "Обожаю изучать новые языки";
+        System.out.println(textLang.substring(textLang.indexOf("изучать")));
+        System.out.println(textLang.substring(textLang.indexOf("изучать"), textLang.indexOf("новые")));
+            }
+    public static void Fourth() {
+        String textIndex = "Домашнее задание не проблема";
+        System.out.println(textIndex.indexOf("не"));
+        System.out.println(textIndex.indexOf("не", textIndex.indexOf("задание")));
+    }
+    }
+
---
+++ MyTests/selenium_maven_test_example-main/src/main/Code/DzSix/DzElevenMain.java
@@ -0,0 +1,16 @@
+package Code.DzSix;
+import Code.AnotherPackage.DzElevenMethods;
+
+public class DzElevenMain {
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+       DzElevenMethods.First();
+        System.out.println('\n'+"Задание 2");
+       DzElevenMethods.Second();
+        System.out.println('\n'+"Задание 3");
+        DzElevenMethods.Third();
+        System.out.println('\n'+"Задание 4");
+       DzElevenMethods.Fourth();
+    }
+}
+
